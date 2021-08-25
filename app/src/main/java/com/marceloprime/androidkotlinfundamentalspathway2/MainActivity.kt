a5b275b972b4aa6e5d43c4acbd295aa9a4ef2701
Marceloprime <marceloreis7777@gmail.com>
2021-08-25 11:50:16 -0300

    commit app do dado

--- app/src/main/java/com/marceloprime/androidkotlinfundamentalspathway2/MainActivity.kt
+++ app/src/main/java/com/marceloprime/androidkotlinfundamentalspathway2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.marceloprime.androidkotlinfundamentalspathway2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,21 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice()}
-        val countUp : Button = findViewById(R.id.countUp)
-        countUp.setOnClickListener{count()}
     }
     private fun rollDice(){
         val resultText: TextView = findViewById(R.id.roll_text)
         val randomInt = (1..6).random()
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
         resultText.text = randomInt.toString()
-        Toast.makeText(this,"button Clicked",Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this,"button Clicked",Toast.LENGTH_SHORT).show()
     }
 
-    private fun count(){
-        val resultText: TextView = findViewById(R.id.roll_text)
-        var number = resultText.text
-        sum = number.toString().toInt() + sum
-        Toast.makeText(this,sum.toString(),Toast.LENGTH_SHORT).show()
-    }
 }
 
